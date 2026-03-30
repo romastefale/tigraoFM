153976a4f910bf1dbf7bb5ebc3781f41c7aa818d
@tigrao <romastefale@gmail.com>
2026-03-30 14:24:45 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -180,7 +180,7 @@ async def deezer_search(query: str):
 # =========================
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Envie uma música ou use @seubot nome")
+    await update.message.reply_text("🔍 Envie uma música ou use @tigraoFMbot nome")
 
 # =========================
 # CHAT SEARCH
